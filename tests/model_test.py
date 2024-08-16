efd47a61940e48aaf4b9f9f6f1fa7997972d87ce
TechyCSR <csr.info.in@gmail.com>
2024-08-16 19:46:09 +0530

    enabling voice module

--- tests/model_test.py
+++ tests/model_test.py
@@ -1,13 +1,22 @@
 
 from g4f.client import Client as GPTClient
+from g4f.cookies import set_cookies
 
+BING_COOKIE ="12xSAr0O1VL29oRLlW4rPVBdlrD2FPtxsUOfbQCOKY-lSxHoNpFK9K0ksvtffUyNeQHgDXQlV4vSDUzP_M26FEEtAJIHU_j97WYirCpGcYT_AKIaIUQYfETPZLQB779hHw-pftDthZ7vfHWk2L7kNM5d5ZsGiGKhGFEctA4tBDhgZMlGwTXDJTsWkUBwtOzDzfOGkg22iD_QcDC0oZLxUlA"
 # Initialize the GPT-4 client
-gpt_client = GPTClient()
+
+gpt_client = GPTClient(provider="You")
+
+# Set Bing cookie
+set_cookies(".bing.com", {
+    "_U": "you.com.har"
+})
 
 
 def get_response(ask):
+        
         response = gpt_client.chat.completions.create(
-            model="gpt-3.5-turbo",
+                model="gpt-4",
             messages=[{"role": "user", "content": ask}]
         )
         return response.choices[0].message.content
