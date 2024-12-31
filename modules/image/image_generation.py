01cb87abf2fa12f2410cb427ab213948d5079121
CHANDAN SINGH <90786270+TechyCSR@users.noreply.github.com>
2024-12-31 08:29:17 +0000

    Img Gen Exception Fixed

--- modules/image/image_generation.py
+++ modules/image/image_generation.py
@@ -16,13 +16,19 @@ mongo_client = MongoClient(DATABASE_URL)
 db = mongo_client['aibotdb']
 user_images_collection = db['user_images']
 
+global error_var
+error_var=0
+
 
 # Function to generate images
 def generate_images(prompt, max_images=5):
+    global error_var
+    error_var=0
     generated_images = 0
     total_attempts = 0
     max_attempts = 2  
     image_urls = []
+
 
     while generated_images < max_images and total_attempts < max_attempts:
         # set_cookies(".bing.com", {
@@ -45,12 +51,13 @@ def generate_images(prompt, max_images=5):
 
         except Exception as e:
             print(f"Error generating image: {str(e)}")
+            error_var=1
             return f"Error generating image: {str(e)}"
 
         total_attempts += 1
 
-    if generated_images < max_images:
-        print(f"Warning: Only generated {generated_images} out of {max_images} requested images.")
+    # if generated_images < max_images:
+    #     print(f"Warning: Only generated {generated_images} out of {max_images} requested images.")
     
     image_urls  = [u.replace("/images/", "./generated_images/") for u in image_urls]
     print(image_urls)
@@ -59,11 +66,14 @@ def generate_images(prompt, max_images=5):
 
 # Telegram bot command handler for generating images
 def generate_command(client, message, prompt):
+    global error_var
     user_id = message.from_user.id
 
     # Generate images
     urls = generate_images(prompt)
-    if not urls:
+
+    if error_var==1:
+        client.send_message(LOG_CHANNEL, f"#ImgLog #Rejected\nImages generated : {prompt}\n**User**: {message.from_user.mention}\n **User ID**: {message.from_user.id} \n **Time** : {datetime.now()} \n**Chat ID**: {message.chat.id}\n")
         message.reply_text("Error generating images. Please try again. or try with different prompt")
         return 
     
@@ -76,7 +86,7 @@ def generate_command(client, message, prompt):
 
     #log images to log channel
     client.send_media_group(LOG_CHANNEL, media_group)
-    client.send_message(LOG_CHANNEL, f"#ImgLog\nImages generated : {prompt}\nUser: {message.from_user.mention}\n user_id: {message.from_user.id} \n time : {datetime.now()} \nchat_id : {message.chat.id} \nuser_id : {message.from_user.id} \n")
+    client.send_message(LOG_CHANNEL, f"#ImgLog\nImages generated : {prompt}\n**User**: {message.from_user.mention}\n **User ID**: {message.from_user.id} \n **Time** : {datetime.now()} \n**Chat ID**: {message.chat.id}\n")
     # delete the image files after sending
     for url in urls:
         os.remove(url)
